75bcc541e8d42f1b3fa26b7d20df28e87b5e9f32
tychejin <tychejin@naver.com>
2022-03-18 16:35:34 +0900

    Feat: Spring for Kafka 기본 - 2. Kafka Topic

---
+++ chap1-clip2/src/main/java/com/fastcampus/chap1clip2/Chap1Clip2Application.java
@@ -0,0 +1,39 @@
+package com.fastcampus.chap1clip2;
+
+import java.util.Collections;
+import java.util.Map;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class Chap1Clip2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Chap1Clip2Application.class, args);
+  }
+
+  @Bean
+  public ApplicationRunner runner(AdminClient adminClient) {
+    return args -> {
+      Map<String, TopicListing> topics = adminClient.listTopics().namesToListings().get();
+      for (String topicName : topics.keySet()) {
+        TopicListing topicListing = topics.get(topicName);
+        System.out.println(topicListing);
+
+        Map<String, TopicDescription> description = adminClient.describeTopics(
+            Collections.singleton(topicName)).all().get();
+        System.out.println(description);
+
+        if (!topicListing.isInternal()) {
+          adminClient.deleteTopics(Collections.singleton(topicName));
+        }
+      }
+    };
+  }
+}
---
+++ chap1-clip2/src/test/java/com/fastcampus/chap1clip2/Chap1Clip2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.chap1clip2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap1Clip2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
